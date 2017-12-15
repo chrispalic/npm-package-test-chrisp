469c499c0c0809379ef6ee282200bf6089f2963d
Chris Palic <chris@chrispalic.com>
2017-12-15 11:43:02 -0500

    Would have been smart to actually export the stuff I want to test.

--- src/npm-package-test-chrisp.ts
+++ src/npm-package-test-chrisp.ts
@@ -1,3 +1,5 @@
 export { Dummy } from './dummy';
 export { EntryPoint } from './entry-point';
 export { TestFunc } from './entry-point';
+export { EntryPoint2 } from './entry-point';
+export { IFileType } from './entry-point';
